ab6b8087aa343db3c0605732dffce0def8b3749e
BrettLennox <brettheaton@hotmail.com>
2022-08-01 17:58:14 +1000

    Adjustments
    also broke some testing things

--- Assets/Crashteroids/Scripts/Game/GameManager.cs
+++ Assets/Crashteroids/Scripts/Game/GameManager.cs
@@ -1,13 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public static int score;
-
-    public static void IncreaseScore(int amount)
+    [System.Serializable]
+    public struct ScoreData
     {
-        score += amount;
+        public int score;
+        public Text scoreText;
+    }
+
+    [SerializeField] private ScoreData _scoreData;
+
+    private void Start()
+    {
+        UpdateScoreText();
+    }
+
+    public void IncreaseScore(int amount)
+    {
+        _scoreData.score += amount;
+        UpdateScoreText();
+    }
+
+    private void UpdateScoreText()
+    {
+        _scoreData.scoreText.text = _scoreData.score.ToString();
     }
 }
